88d49c90c0161f3d80d96ab8824cae5f59406881
yanzhang000 <circusburger63@yahoo.com>
2018-03-04 01:36:36 -0500

    implemented adding notes

--- src/app/modals/add-post-it-note/add-post-it-note.component.ts
+++ src/app/modals/add-post-it-note/add-post-it-note.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Color} from '../../models/Color';
+import {Note} from '../../models/Note';
 
 @Component({
   selector: 'app-add-post-it-note',
@@ -8,7 +9,11 @@ import {Color} from '../../models/Color';
 })
 export class AddPostItNoteComponent implements OnInit {
 
+  @Output() userAction: EventEmitter<any> = new EventEmitter();
   public colors = Color;
+  public notes = '';
+  public notesTitle = '';
+  public selectedColor = Color.RED;
 
   constructor() { }
 
@@ -16,7 +21,24 @@ export class AddPostItNoteComponent implements OnInit {
   }
 
   public setColor () {
-    return { 'background-color': 'green' };
+    return { 'background-color': this.selectedColor };
+  }
+
+  public notesOnChange () {
+
+  }
+
+  public setSelectedColor (color) {
+    this.selectedColor = color;
+  }
+
+  public cancel () {
+    this.userAction.emit('cancel');
+  }
+
+  public addNote () {
+    const note = new Note(this.notesTitle, this.notes, this.selectedColor);
+    this.userAction.emit(note);
   }
 
 }
